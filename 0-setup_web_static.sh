4219e7e567d4d724be10cb01663a29c22ce1157a
Amy <amy.tai0120@gmail.com>
2018-07-30 23:03:55 +0000

    messed up tab

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 
 sudo apt-get update
 sudo apt-get install Nginx -y
-mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 echo "Holbie" | sudo tee /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
+sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-content="\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
+content="\\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
 sudo sed -i "38i $content" /etc/nginx/sites-available/default
 sudo service nginx restart
