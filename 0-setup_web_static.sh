2772a4eb8a17422369c5b6e31affca36ad1faa0f
Amy <amy.tai0120@gmail.com>
2018-07-30 22:50:43 +0000

    mistake fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,8 @@ sudo apt-get install Nginx -y
 mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 echo "Holbie" | sudo tee /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
-sudo ln -s /data/web_static/current /data/web_static/releases/test/
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-content="location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
+content="\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
 sudo sed -i "38i $content" /etc/nginx/sites-available/default
 sudo service nginx restart
