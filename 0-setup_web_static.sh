49358bcc650187c6f4564a0bc43b537816949ee4
Amy <amy.tai0120@gmail.com>
2018-07-30 23:12:09 +0000

    chmod for the kill

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # set up for web server deployment
 
-sudo apt-get update
-sudo apt-get install Nginx -y
-sudo mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo apt-get -y update
+sudo apt-get install -y Nginx
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 echo "Holbie" | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-content="\\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
-sudo sed -i "38i $content" /etc/nginx/sites-available/default
+content="location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}"
+sudo sed -i "39i $content" /etc/nginx/sites-available/default
 sudo service nginx restart
